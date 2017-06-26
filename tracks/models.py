2e9263f41d537163d173651bd8722e476c94a627
Dommccas <dominic.mccaskill@gmail.com>
2017-06-26 10:25:54 +0000

    Removed bio, age and location from user profile

--- tracks/models.py
+++ tracks/models.py
@@ -93,9 +93,6 @@ class Distance_Unit(models.Model):
 class Profile(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(max_length=500, blank=True)
-    location = models.CharField(max_length=30, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
     distance_unit = models.ForeignKey(Distance_Unit, null=True, blank=True)
     pace = models.BooleanField(default=True)
 
