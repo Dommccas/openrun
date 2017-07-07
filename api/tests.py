a52d3b08d1e22c89abdbda5fb4beb277842d041d
Dommccas <dominic.mccaskill@gmail.com>
2017-07-07 11:02:59 +0000

    added tests for gpx file checks

--- api/tests.py
+++ api/tests.py
@@ -69,6 +69,21 @@ class uploadTestCase(TestCase):
                 </trk>\
                 </gpx>'.encode('utf-8')
 
+        invalid_gpx = '<?xml version="1.0" encoding="UTF-8"?>\
+                <gpx version="1.1">\
+                <trk>\
+                <name><![CDATA[Running 4/15/10 6:04 pm]]></name>\
+                <time>2010-04-15T18:04:28Z</time>\
+                <trkseg>\
+                <trkpt lat="51.443217000" lon="-2.610013000">\
+                <ele>69.7</ele>\
+                <trkpt lat="51.443255000" lon="-2.615090000">\
+                <ele>81.0</ele>\
+                <time>2010-04-15T18:07:33Z</time>\
+                </trkpt>\
+                </trk>\
+                </gpx>'.encode('utf-8')
+
         self.staff_user = User.objects.create(
             username='staff',
             is_staff=True,)
@@ -77,10 +92,31 @@ class uploadTestCase(TestCase):
             is_staff=False,)
         self.client = APIClient()
 
-        self.file_dict = { 'gpx_file': SimpleUploadedFile(
+        self.file_dict_1 = {'file': SimpleUploadedFile(
             'test.gpx', valid_gpx, 'text/xml')}
+        self.file_dict_2 = {'file': SimpleUploadedFile(
+            'test.gpx', valid_gpx, 'text/xml')}
+        self.file_dict_3 = {'file': SimpleUploadedFile(
+            'test.gpx', invalid_gpx, 'text/xml')}
+        self.file_dict_4 = {'file': SimpleUploadedFile(
+            'test.gpx', invalid_gpx, 'virus/trojan')}
+        self.client.force_authenticate(user=self.staff_user)
 
     def test_api_can_upload_file(self):
-        self.client.force_authenticate(user=self.staff_user)
-        resp = self.client.put(reverse('upload', {'filename': 'test.gpx'}), self.file_dict, format='multipart')
+
+        resp = self.client.post(reverse('upload'), self.file_dict_1)
         self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+    def test_api_cant_upload_file_twice(self):
+        resp = self.client.post(reverse('upload'), self.file_dict_1)
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        resp = self.client.post(reverse('upload'), self.file_dict_2)
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_api_cant_upload_invalid_gpx_file(self):
+        resp = self.client.post(reverse('upload'), self.file_dict_3)
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_api_cant_upload_invalid_file_type(self):
+        resp = self.client.post(reverse('upload'), self.file_dict_4)
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
