52b98dc909b116627b9b76efd43d35765ae8ba1c
Dommccas <dominic.mccaskill@gmail.com>
2017-07-14 08:32:25 +0000

    Added ability to provide svg height and width as url param

--- api/renderers.py
+++ api/renderers.py
@@ -4,9 +4,11 @@ from django.contrib.gis.geos import GEOSGeometry
 from django.template import loader
 
 
-class TemplateRenderer(BaseRenderer):
+class SVGRenderer(BaseRenderer):
 
-    template_name = None
+    media_type = 'image/svg+xml'
+    format = 'svg'
+    template_name = 'api/track.svg'
 
     def render(self, data, accepted_media_type=None, renderer_context=None):
 
@@ -19,11 +21,16 @@ class TemplateRenderer(BaseRenderer):
 
         for feature in features:
 
-            height = 1000
-            width = 1000
+            try:
+                height = int(renderer_context['request'].query_params['height'])
+                width = int(renderer_context['request'].query_params['width'])
+            except:
+                height = 1000
+                width = 1000
 
-            Geometry = GEOSGeometry(str(feature['geometry']))
-            extent = Geometry.extent
+
+            geometry = GEOSGeometry(str(feature['geometry']))
+            extent = geometry.extent
             translate_x = extent[0] * -1
             translate_y = extent[1] * -1
 
@@ -33,9 +40,9 @@ class TemplateRenderer(BaseRenderer):
             max_range = max(range_x, range_y)
 
             translate = 'translate(%s %s)' % ( translate_x, translate_y )
-            scale = 'scale(%s)' % ( max(height, width) / max_range )
+            scale = 'scale(%s)' % ( min(height, width) / max_range )
 
-            for line in Geometry:
+            for line in geometry:
                 tmpPath = 'M ' + str(line[0][0]) + ' ' + str(line[0][1])
                 for point in line[1:]:
                     tmpPath += ' L ' + str(point[0]) + ' ' + str(point[1])
@@ -52,8 +59,4 @@ class TemplateRenderer(BaseRenderer):
             })
 
 
-class SVGRenderer(TemplateRenderer):
 
-    media_type = 'image/svg+xml'
-    format = 'svg'
-    template_name = 'api/track.svg'
